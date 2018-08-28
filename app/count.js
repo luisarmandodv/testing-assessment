b5ea63568cb8107665adc91aa1d56ff6124d847f
luisarmandodv <luisarmandodv@gmail.com>
2018-08-28 10:46:54 -0500

    Add count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,30 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+  count: function(start, end) {
+    var timeout;
+    let time;
+    function doIt() {
+      console.log(start++);
 
+      if (start <= end) {
+        timeout = setTimeout(doIt, 100);
+      }
+    }
+
+    async function showNumbers() {
+      if (start <= end) {
+        console.log(start);
+        await time = setTimeout(showNumbers, 100);
+      }
+    }
+
+    showNumbers();
+
+    return {
+      cancel: function() {
+        time && clearTimeout(time);
+      }
+    };
   }
 };
