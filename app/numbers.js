1e5784b2cd725f28be33468757a0c551d2dbf574
luisarmandodv <luisarmandodv@gmail.com>
2018-08-28 18:45:56 -0500

    Add numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    let binary = num.toString(2);
+    return parseInt(binary[binary.length - bit], 10);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    function pad(s) {
+      while (s.length < 8) s = '0' + s;
+      return s;
+    }
 
+    return pad(num.toString(2));
   },
 
   multiply: function(a, b) {
-
+    return parseFloat((a * b).toFixed(b.toString().length));
   }
 };
