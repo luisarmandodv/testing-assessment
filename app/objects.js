b4c0b9fb514e0aaa1e558fdbe8226b14f04cf8e4
luisarmandodv <luisarmandodv@gmail.com>
2018-08-28 23:40:57 -0500

    Add objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+    array = [];
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        array.push(key + ': ' + obj[key]);
+      }
+    }
 
+    return array;
   }
 };
