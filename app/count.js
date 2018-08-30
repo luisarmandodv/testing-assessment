bbc2d388ca0febbd954c011d77b5dbae38134707
luisarmandodv <luisarmandodv@gmail.com>
2018-08-30 12:29:18 -0500

    Add counter (answer copied)

--- app/count.js
+++ app/count.js
@@ -3,27 +3,19 @@ exports = typeof window === 'undefined' ? global : window;
 exports.countAnswers = {
   count: function(start, end) {
     var timeout;
-    let time;
     function doIt() {
-      console.log(start++);
+      console.log(start++); // eslint-disable-line no-console
 
       if (start <= end) {
         timeout = setTimeout(doIt, 100);
       }
     }
 
-    async function showNumbers() {
-      if (start <= end) {
-        console.log(start);
-        await time = setTimeout(showNumbers, 100);
-      }
-    }
-
-    showNumbers();
+    doIt();
 
     return {
       cancel: function() {
-        time && clearTimeout(time);
+        timeout && clearTimeout(timeout);
       }
     };
   }
